3fcd7ceeedd157f0e34f62bc1f05eedfac575b3d
air <92066902+qitoqito@users.noreply.github.com>
2022-11-24 17:33:22 +0800

    1.0.5

--- setup.py
+++ setup.py
@@ -2,16 +2,14 @@
 # -*- coding: utf-8 -*-
 """
 @File    : setup.py
-@Time    : 2022/9/27 下午8:54  
+@Time    : 2022/9/27 下午8:54
 """
-# !/usr/bin/env python3
-# -*- coding:UTF-8 -*-
 from setuptools import setup
 
 import os, codecs, platform
 from qito.config.profile import version
 
-REQ = ["requests", "pysocks", "coloredlogs", "pyexecjs"]
+REQ = ["requests", "pysocks", "coloredlogs", "pyexecjs", "pyaes"]
 
 
 def find_packages(*tops):
@@ -20,7 +18,6 @@ def find_packages(*tops):
         for root, dirs, files in os.walk(d, followlinks=True):
             if "__init__.py" in files:
                 packages.append(root)
-        # packages.extend(['vparse/temp',"vparse/cookie"])
     return packages
 
 
@@ -33,7 +30,7 @@ setup(
     license="MIT",
     description="视频音乐下载播放器",
     packages=find_packages("qito"),
-    package_data={"qito": ["parse/json/*.json", "parse/javascript/*.*"]},
+    package_data={"qito": ["tool/javascript/*.*"]},
     python_requires=">=3.5",
     long_description="""基于Python,视频、音乐、直播下载器,支持YouKu、QQ、Iqiyi等""",
     classifiers=["License :: OSI Approved :: MIT License"],
