15edc6af2f33a0a7b7d1ae85054c1cea1d994ebe
air <92066902+qitoqito@users.noreply.github.com>
2022-11-24 17:33:15 +0800

    Update profile.py

--- qito/config/profile.py
+++ qito/config/profile.py
@@ -4,5 +4,5 @@
 @File    : profile.py
 @Time    : 2022/2/13 下午4:48
 """
-version = "1.0.4"
+version = "1.0.5"
 jsonFilter = "version|cookie|dir|exit|format|info|json|query|debug|proxy|http|timeout|download|merge|multi|dir|capture|geometry|loop|player|fullscreen|start|end|length|no_audio|no_video|volume|init|language|itag|ccode|password|encoder|name|context"
